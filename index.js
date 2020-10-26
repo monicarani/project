4e5ceb0394aab442028f2232212ece6648145dcf
monicarani <72614310+monicarani@users.noreply.github.com>
2020-10-26 16:06:22 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var person = prompt("Please enter your name", "");
-    if (person != null) {
-        document.getElementById("menu").style.display="block";
-        document.getElementById("demo").innerHTML =
-        "Hello " + person.toLocaleUpperCase() + " ! <br>So nice to see you here.** Hope u enjoy the service ****<br>Please click the below <strong>MENU Button</strong> to get the menu... <br>";
-    }    
-}
