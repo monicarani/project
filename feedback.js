2b7f5516289b34efb54b21a3538c805ec05fa09d
monicarani <72614310+monicarani@users.noreply.github.com>
2020-10-26 21:42:02 +0530

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var feedback = prompt("Please enter your feedback", " ");
+    if (feedback != null) {
+    document.getElementById("demo").innerHTML =" THANK YOU FOR YOUR FEED BACK  ";
+       }
+}
---
+++ menu.js
@@ -0,0 +1,88 @@
+function deleteRow(r) {
+  var i = r.parentNode.parentNode.rowIndex;
+  document.getElementById("myTable").deleteRow(i);
+}
+
+function myFunction1() {
+  var row = document.getElementById("my1Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction2() {
+  var row = document.getElementById("my2Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction3() {
+  var row = document.getElementById("my3Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction4() {
+  var row = document.getElementById("my4Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction5() {
+  var row = document.getElementById("my5Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction6() {
+  var row = document.getElementById("my6Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction7() {
+  var row = document.getElementById("my7Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunctio8() {
+  var row = document.getElementById("my8Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction9() {
+  var row = document.getElementById("my9Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction10() {
+  var row = document.getElementById("my10Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction11() {
+  var row = document.getElementById("my11Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction12() {
+  var row = document.getElementById("my12Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction13() {
+  var row = document.getElementById("my13Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
+
+function myFunction14() {
+  var row = document.getElementById("my14Row");
+  var x = row.insertCell(0);
+  x.innerHTML = "+1 quantity";
+}
