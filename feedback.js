569796b018287a57c246558a1fd987b3314b4104
monicarani <72614310+monicarani@users.noreply.github.com>
2020-10-31 14:41:36 +0000

    Delete feedback.js

--- feedback.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    var feedback = prompt("Please enter your feedback", " ");
-    if (feedback != null) {
-    document.getElementById("demo").innerHTML =" THANK YOU FOR YOUR FEED BACK  ";
-       }
-}
